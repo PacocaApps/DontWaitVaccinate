4d5afa70457dfcb8f4c005b5ced66e2c2bd2ee5c
PacocaApps <gpaaadm@gmail.com>
2019-05-30 07:45:07 -0300

    UpdATEniGGA
    bLyat Niggalicious

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+var cnv;
+var Balls = [];
+var numBalls = 100;
 function setup(){
 
    
- 
-    var cnv = createCanvas(windowHeight, windowWidth);
-    cnv.parent("bgcc");
-    background(255);
+ cnv = createCanvas(windowWidth, windowHeight);
+  cnv.position(0, 0);
+
+    background(0);
+    for(var i =0;i <100;i++){
+        
+        Balls[i] = new Ball(random(width),random(height),random(10))
+    }
+    
 
 }
 
 function draw(){
-
-
+ background(0);
+    for(var i =0;i <1000;i++){
+        
+        Balls[i].display();
+    }
 
 
 }
 
-class Balls{
+class Ball{
 
 constructor(x,y,sz){
 
@@ -28,10 +39,15 @@ constructor(x,y,sz){
 
 }
 display(){
-fill(255,0,0);
+noStroke();
+    fill(255,0,0);
 ellipse(this.x,this.y,this.size,this.size)
 
 
 }
 
+}
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
 }
